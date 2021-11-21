b926d69d7ec3704d1577c0308461147e28df597d
Hugo Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-11-21 20:06:42 +0000

    Comments

--- main.java
+++ main.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 class Main {
+  //main function which executes the main method
   public static void main(String[] args) {
     question();
     System.exit(0);
-  }
+  } // END of main
 
+  //this method questions the user about train details
   public static void question ()
   {
+    //declaring variables needed for loop
     String station;
     String earlyStation = "";
     int mostPunctual = 999;
@@ -15,20 +18,28 @@ class Main {
     int total = 0;
     boolean end = false;
 
+    //loop asks questions until end is true
     while (end == false) {
+
+      //aks questions and returns strings
       station = getString("What is the destination of the train that just departed?");
+
+      //if xxx is entered then end becomes true
       if (station.equalsIgnoreCase("xxx")) {
         System.out.println(" ");
-        end = true;
+        end = true; // ends the while loop
       } else {
 
+        //asks question and returns int
         newLate = getInt("How many minutes late was it?");
         
-        total = total + newLate;
+        total = total + newLate; // tracker for cumlative minutes late
         System.out.println("");
 
+        //compares new late value, if smaller...
         if (newLate < mostPunctual) 
         {
+          //replaces mostPunctual and earlyStation variables with more recent newLate and station
           mostPunctual = newLate;
           earlyStation = station;
         }
@@ -37,15 +48,18 @@ class Main {
 
     }
 
+    //message emthod to produce message with variables retreived from questions
     message(total, earlyStation, mostPunctual);
-  }
+  } // END of question
 
+  //outputs message using variables
   public static void message (int total, String station, int late) 
   {
     System.out.println("The trains were in total "+ total +" minutes late. ");
     System.out.println("The most punctual train was to "+ station +". It was "+ late +" minute late.");
-  }
+  }//END of message
 
+  //outputs argument and takes in String which is returned
   public static String getString (String x)
   {
     Scanner scanner = new Scanner(System.in);
@@ -53,14 +67,15 @@ class Main {
     String station = scanner.nextLine();
     return station;
 
-  }
+  }//END of getString
 
+  //outputs argument and takes in value which is returned
   public static int getInt (String x)
   {
     Scanner scanner = new Scanner(System.in);
     System.out.print(x + " ");
     int newLate = scanner.nextInt();
     return newLate;
-  }
+  }//END of getInt
 
 }
